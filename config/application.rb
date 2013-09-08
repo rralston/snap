4e9067c2dbb38c56b1aa4657278b84e5f7afedfb
Denis Grankin <liquidautumn@gmail.com>
2013-09-08 23:20:58 +0300

    added correct webfonts, few minor tweaks

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,9 @@ module Snap
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    #Vendor fonts
+    config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
 
     #Fix for Heroku (remove when upgrade to Rails 4)
     config.assets.initialize_on_precompile = false
