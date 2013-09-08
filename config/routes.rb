523202bdfec6c83437da44ae71bb783f556f43c6
Sri Vishnu Totakura <t.srivishnu@gmail.com>
2013-09-08 13:39:44 +0530

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Snap::Application.routes.draw do
+  root :to => 'clips#index'
   resources :clips
   match 'clips/:id/share' => 'clips#share'
 
@@ -51,7 +52,6 @@ Snap::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'snaps#index'
 
   # See how all your routes lay out with "rake routes"
 
